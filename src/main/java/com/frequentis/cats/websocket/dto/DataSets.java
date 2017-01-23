bd8403eec6d7ef041b407d13caa4e787312420a3
AYAR Mehmet <Mehmet.AYAR@frequentis.com>
2017-01-23 10:21:58 +0100

    QXVP-1724: Websocket domain package for XVP team

---
+++ src/main/java/com/frequentis/cats/websocket/dto/DataSets.java
@@ -0,0 +1,18 @@
+/*
+ * COPYRIGHT: FREQUENTIS AG. All rights reserved.
+ *            Registered with Commercial Court Vienna,
+ *            reg.no. FN 72.115b.
+ */
+package com.frequentis.cats.websocket.dto;
+
+/**
+ * Created by MAyar on 18.01.2017.
+ */
+public class DataSets {
+
+    /**
+     * Used for naming parameters.
+     */
+    public static final String Named = "named";
+
+}
