e9502a3594039535dce44906188a5aef3bf2521e
AYAR Mehmet <Mehmet.AYAR@frequentis.com>
2017-04-28 12:32:49 +0200

    CATS websocket domain package for XVP AG/GG team

--- src/main/java/com/frequentis/cats/websocket/automation/steps/WebsocketClientLocalSteps.java
+++ src/main/java/com/frequentis/cats/websocket/automation/steps/WebsocketClientLocalSteps.java
@@ -5,55 +5,147 @@
  */
 package com.frequentis.cats.websocket.automation.steps;
 
-import com.frequentis.c4i.test.agent.websocket.client.impl.models.ClientEndpointConfiguration;
-import com.frequentis.c4i.test.bdd.fluent.step.local.LocalStep;
-import com.frequentis.c4i.test.model.ExecutionDetails;
-import com.frequentis.cats.websocket.dto.WebsocketAutomationSteps;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.slf4j.LoggerFactory;
 
-import java.util.Map;
+import com.frequentis.c4i.test.agent.websocket.client.impl.models.ClientEndpointConfiguration;
+import com.frequentis.c4i.test.bdd.fluent.step.local.LocalStep;
+import com.frequentis.c4i.test.model.ExecutionDetails;
+import com.frequentis.c4i.test.model.parameter.CatsCustomParameterBase;
+import com.frequentis.cats.websocket.dto.WebsocketAutomationSteps;
 
 /**
  * Created by MAyar on 18.01.2017.
  */
-public class WebsocketClientLocalSteps extends WebsocketAutomationSteps {
-    private static org.slf4j.Logger LOG = LoggerFactory.getLogger(WebsocketClientLocalSteps.class.getName());
+public class WebsocketClientLocalSteps extends WebsocketAutomationSteps
+{
+   private static final org.slf4j.Logger LOG = LoggerFactory.getLogger( WebsocketClientLocalSteps.class.getName() );
 
-    @Given("named the websocket configurations: $connections")
-    public void namedWebSocketClientConnection(final Map<String, ClientEndpointConfiguration> connections) {
-        LOG.info ("Connections: " + connections);
-        LocalStep step = localStep("Added named websocket client configurations to user named parameters");
-        int count = 1;
-        for (Map.Entry<String, ClientEndpointConfiguration> connectionsEntry : connections.entrySet()) {
-            String key = connectionsEntry.getKey();
-            ClientEndpointConfiguration config = connectionsEntry.getValue();
-            setStoryListData(key, config);
-            step.details(ExecutionDetails.create("Parse data table. Entry " + count + " of " + connections.size())
-                    .expected("ExamplesTable can be parsed")
-                    .receivedData(key, config)
-                    .success(true));
-            count++;
-        }
-        evaluate(step);
-    }
+   private static final Integer ZERO_PADDING_FLAG_AND_WIDTH_START_INDEX = 3;
 
-    @Then("wait for $secs seconds")
-    public void waitForSeconds(final int secs) {
-        LocalStep step = localStep("Wait for " + secs + " seconds");
 
-        try {
-            Thread.sleep(secs * 1000);
-            step.details(ExecutionDetails.create("Wait for " + secs + " seconds")
-                        .received("Waited")
-                        .success(true));
-        } catch (Exception ex) {
-            LOG.error("Error", ex);
-            step.details(ExecutionDetails.create("Wait for " + secs + " seconds")
-                    .received("Waited with error")
-                    .success(false));
-        }
-        record(step);
-    }
+   @Given("named the websocket configurations: $connections")
+   public void namedWebSocketClientConnection( final Map<String, ClientEndpointConfiguration> connections )
+   {
+      LOG.info( "Connections: " + connections );
+      final LocalStep step = localStep( "Added named websocket client configurations to user named parameters" );
+      int count = 1;
+      for ( final Map.Entry<String, ClientEndpointConfiguration> connectionsEntry : connections.entrySet() )
+      {
+         final String key = connectionsEntry.getKey();
+         final ClientEndpointConfiguration config = connectionsEntry.getValue();
+         setStoryListData( key, config );
+         step.details( ExecutionDetails.create( "Parse data table. Entry " + count + " of " + connections.size() )
+               .expected( "ExamplesTable can be parsed" ).receivedData( key, config ).success( true ) );
+         count++;
+      }
+      evaluate( step );
+   }
+
+
+   @Then("wait for $secs seconds")
+   public void waitForSeconds( final int secs )
+   {
+      final LocalStep step = localStep( "Wait for " + secs + " seconds" );
+
+      try
+      {
+         Thread.sleep( secs * 1000 );
+         step.details(
+               ExecutionDetails.create( "Wait for " + secs + " seconds" ).received( "Waited" ).success( true ) );
+      }
+      catch ( final Exception ex )
+      {
+         LOG.error( "Error", ex );
+         step.details( ExecutionDetails.create( "Wait for " + secs + " seconds" ).received( "Waited with error" )
+               .success( false ) );
+      }
+      record( step );
+   }
+
+
+   @Given("The $namedParameter is defined as $value")
+   public void setNamedParameter( final String namedParameter, final String value )
+   {
+      final LocalStep setNamedParameter = localStep( "Named Parameter" );
+
+      setStoryListData( namedParameter, value );
+
+      setNamedParameter.details( ExecutionDetails.create( "Parameter named" ).usedData( namedParameter, value ) );
+   }
+
+
+   /*
+   
+   | key                                                                                                                                 |
+   | {"header": {"correlationId": "00004711-0815-4000-8000-00000000021<%i>"}, "body": {"statisticRequest": { "sessionId": "ID<%i>"}}}    |
+   
+   */
+
+   @Given("$amount messages created in group $group with varaible field with value starting from $startIndex : $messages")
+   public void createNamedParameter( final Integer amount, final String group, final Integer startIndex,
+         final ArrayList<CatsCustomParameterBase> messages )
+   {
+      final LocalStep createNamedParameter = localStep( "Creating named group" );
+
+      final ArrayList<String> messageList = new ArrayList<>();
+      final ArrayList<String> finalMessageList = new ArrayList<>();
+      for ( final CatsCustomParameterBase parameter : messages )
+      {
+         final String message = parameter.getKey();
+         LOG.info( "message is " + message );
+         messageList.add( message );
+
+      }
+
+      for ( int i = startIndex; i <= amount + startIndex; i++ )
+      {
+         LOG.info( "messageList is : " + messageList );
+         for ( final String message : messageList )
+         {
+            finalMessageList.add( replaceIndex( message, i ) );
+         }
+      }
+
+      setStoryListData( group, finalMessageList );
+      createNamedParameter.details( ExecutionDetails.create( "Group created" ).usedData( group, finalMessageList ) );
+
+   }
+
+
+   private String replaceIndex( final String stringValue, final Integer replaceIndex )
+   {
+      String output = stringValue;
+
+      // <%i[0-9]*> matches values like <%i>, <%i2>, <%i03>.
+      final Matcher matcher = Pattern.compile( "<%i[0-9]*>" ).matcher( stringValue );
+      boolean anyMatch = false;
+      while ( matcher.find() )
+      {
+         anyMatch = true;
+         final String matchedString = stringValue.substring( matcher.start(), matcher.end() );
+
+         // E.g.: 2 means: The target width is 2.
+         // E.g.: 02 means: 0 indicates a zero-padding and 2 indicates the width.
+         final String zeroPaddingFlagAndWidth =
+               matchedString.substring( ZERO_PADDING_FLAG_AND_WIDTH_START_INDEX, matchedString.length() - 1 );
+
+         final String index = String.format( "%" + zeroPaddingFlagAndWidth + "d", replaceIndex );
+         output = output.replace( matchedString, index );
+      }
+
+      if ( !anyMatch )
+      {
+         // If the string value did not contain any replace indicator, the index will be added to the end of the string.
+         output += replaceIndex;
+      }
+
+      return output;
+   }
 }
--- src/main/java/com/frequentis/cats/websocket/automation/steps/WebsocketClientRemoteSteps.java
+++ src/main/java/com/frequentis/cats/websocket/automation/steps/WebsocketClientRemoteSteps.java
@@ -5,6 +5,14 @@
  */
 package com.frequentis.cats.websocket.automation.steps;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
 import com.frequentis.c4i.test.agent.websocket.client.impl.models.ClientEndpointConfiguration;
 import com.frequentis.c4i.test.bdd.fluent.step.local.LocalStep;
 import com.frequentis.c4i.test.bdd.fluent.step.remote.RemoteStep;
@@ -16,141 +24,158 @@ import com.frequentis.c4i.test.model.parameter.CatsCustomParameterBase;
 import com.frequentis.cats.websocket.automation.model.ProfileToWebSocketConfigurationReference;
 import com.frequentis.cats.websocket.dto.BookableProfileName;
 import com.frequentis.cats.websocket.dto.WebsocketAutomationSteps;
-import org.jbehave.core.annotations.Given;
-import org.jbehave.core.annotations.Then;
-import org.jbehave.core.annotations.When;
+
 import scripts.cats.websocket.parallel.OpenWebSocketClientConnection;
 import scripts.cats.websocket.parallel.SendTextMessageAsIsParallel;
 import scripts.cats.websocket.sequential.ReceiveMessageAsIs;
 import scripts.cats.websocket.sequential.SendTextMessageAsIs;
 import scripts.cats.websocket.sequential.StartClientConnectionRecording;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by MAyar on 18.01.2017.
  */
-public class WebsocketClientRemoteSteps extends WebsocketAutomationSteps {
+public class WebsocketClientRemoteSteps extends WebsocketAutomationSteps
+{
 
-    @Given("applied the websocket configuration: $references")
-    public void openNamedWebSocketConnections(final List<ProfileToWebSocketConfigurationReference> namedProfileToWebSocketConfigReferences) {
-        RemoteStep remStep = remoteStep("Parsing Datatables paralelly");
-        RemoteStepExecution execution = null;
-        if (namedProfileToWebSocketConfigReferences.size() == 0) {
-            localStep("looking into tables for parsing")
-                    .details(ExecutionDetails.create("Parsing Table")
-                            .expected("Table contains at least one entry")
-                            .received("NO ENTRY FOUND " + namedProfileToWebSocketConfigReferences)
-                            .success(false));
-        }
+   @Given("applied the websocket configuration: $references")
+   public void openNamedWebSocketConnections(
+         final List<ProfileToWebSocketConfigurationReference> namedProfileToWebSocketConfigReferences )
+   {
+      final RemoteStep remStep = remoteStep( "Parsing Datatables paralelly" );
+      RemoteStepExecution execution = null;
+      if ( namedProfileToWebSocketConfigReferences.size() == 0 )
+      {
+         localStep( "looking into tables for parsing" )
+               .details( ExecutionDetails.create( "Parsing Table" ).expected( "Table contains at least one entry" )
+                     .received( "NO ENTRY FOUND " + namedProfileToWebSocketConfigReferences ).success( false ) );
+      }
 
-        int i = 1;
-        for (ProfileToWebSocketConfigurationReference reference : namedProfileToWebSocketConfigReferences) {
-            // add the named reference between websocket config and profile to the user named parameters
-            setStoryListData(reference.getKey(), reference);
-            ExecutionData data = new ExecutionData();
-            localStep("Parsing Table")
-                    .details(ExecutionDetails.create("Parse data table. Entry " + i++ + " of " + namedProfileToWebSocketConfigReferences.size())
-                            .expected("ExamplesTable can be parsed")
-                            .receivedData(reference.getKey(), reference)
-                            .success(true));
-            final ClientEndpointConfiguration config = getStoryListData(reference.getWebSocketConfigurationName(), ClientEndpointConfiguration.class);
+      int i = 1;
+      for ( final ProfileToWebSocketConfigurationReference reference : namedProfileToWebSocketConfigReferences )
+      {
+         // add the named reference between websocket config and profile to the user named parameters
+         setStoryListData( reference.getKey(), reference );
+         final ExecutionData data = new ExecutionData();
+         localStep( "Parsing Table" ).details( ExecutionDetails
+               .create( "Parse data table. Entry " + i++ + " of " + namedProfileToWebSocketConfigReferences.size() )
+               .expected( "ExamplesTable can be parsed" ).receivedData( reference.getKey(), reference )
+               .success( true ) );
+         final ClientEndpointConfiguration config =
+               getStoryListData( reference.getWebSocketConfigurationName(), ClientEndpointConfiguration.class );
 
-            ArrayList<String> endpointName = new ArrayList<String>();
-            endpointName.add(reference.getKey());
+         final ArrayList<String> endpointName = new ArrayList<String>();
+         endpointName.add( reference.getKey() );
 
-            execution =
-                    remStep.scriptOn(
-                            profileScriptResolver()
-                                    .map(OpenWebSocketClientConnection.class, BookableProfileName.websocket),
-                            requireProfile(reference.getProfileName()))
-                            .input(OpenWebSocketClientConnection.IPARAM_ENDPOINTCONFIGURATION, (Serializable) config)
-                            .input(OpenWebSocketClientConnection.IPARAM_MULTIPLEENDPOINTNAMES, endpointName);
-        }
+         execution =
+               remStep
+                     .scriptOn( profileScriptResolver().map( OpenWebSocketClientConnection.class,
+                           BookableProfileName.websocket ), requireProfile( reference.getProfileName() ) )
+                     .input( OpenWebSocketClientConnection.IPARAM_ENDPOINTCONFIGURATION, ( Serializable ) config )
+                     .input( OpenWebSocketClientConnection.IPARAM_MULTIPLEENDPOINTNAMES, endpointName );
+      }
 
-        if (execution != null) {
-            evaluate(execution);
-        }
-    }
+      if ( execution != null )
+      {
+         evaluate( execution );
+      }
+   }
 
-    @Then("start recording on named websocket $namedWebSocket with a buffer size of $bufferSize")
-    public void startRecordingOnNamedWebSocket(final String namedWebSocket, final Integer bufferSize) {
-        ProfileToWebSocketConfigurationReference reference = getStoryListData(namedWebSocket, ProfileToWebSocketConfigurationReference.class);
 
-        evaluate(
-                remoteStep("Start recording on named WebSocket: " + namedWebSocket).scriptOn(
-                        profileScriptResolver()
-                                .map(
-                                        StartClientConnectionRecording.class, BookableProfileName.websocket),
-                        requireProfile(reference.getProfileName()))
-                        .input(StartClientConnectionRecording.IPARAM_ENDPOINTNAME, reference.getKey())
-                        .input(StartClientConnectionRecording.IPARAM_BUFFERSIZE, bufferSize)
-        );
-    }
+   @Then("start recording on named websocket $namedWebSocket with a buffer size of $bufferSize")
+   public void startRecordingOnNamedWebSocket( final String namedWebSocket, final Integer bufferSize )
+   {
+      final ProfileToWebSocketConfigurationReference reference =
+            getStoryListData( namedWebSocket, ProfileToWebSocketConfigurationReference.class );
 
-    @When("using the websocket $namedWebSocket the message named $namedMessage is sent as is with time stamp $namedTimeStamp")
-    public void sendMessageAsIsOverNamedWebSocketAndNameMessage(final String namedWebSocket, final String namedMessage, final String namedTimeStamp) {
-        ProfileToWebSocketConfigurationReference reference = getStoryListData(namedWebSocket, ProfileToWebSocketConfigurationReference.class);
-        String messageToSend = assertStoryListData(namedMessage, String.class);
-        RemoteStepResult remoteStepResult = evaluate(
-                remoteStep("Sending message with using " + namedWebSocket)
-                        .scriptOn(profileScriptResolver()
-                                        .map(
-                                                SendTextMessageAsIs.class, BookableProfileName.websocket),
-                                requireProfile(reference.getProfileName()))
-                        .input(SendTextMessageAsIs.IPARAM_ENDPOINTNAME, namedWebSocket)
-                        .input(SendTextMessageAsIs.IPARAM_MESSAGETOSEND, messageToSend)
-        );
-        setStoryListData(namedTimeStamp, remoteStepResult.getOutput(SendTextMessageAsIs.OPARAM_ACTIONTIME));
+      evaluate(
+            remoteStep( "Start recording on named WebSocket: " + namedWebSocket )
+                  .scriptOn( profileScriptResolver().map( StartClientConnectionRecording.class,
+                        BookableProfileName.websocket ), requireProfile( reference.getProfileName() ) )
+                  .input( StartClientConnectionRecording.IPARAM_ENDPOINTNAME, reference.getKey() )
+                  .input( StartClientConnectionRecording.IPARAM_BUFFERSIZE, bufferSize ) );
+   }
 
-    }
 
-    @When("using the websocket $namedWebSocket the messages are sent: $namedMessageList")
-    public void sendMessagesAsIsOverNamedWebSocket(final String namedWebSocket, final List<CatsCustomParameterBase> namedMessageList) {
-        ProfileToWebSocketConfigurationReference reference = getStoryListData(namedWebSocket, ProfileToWebSocketConfigurationReference.class);
-        List<String> messageToSendList= new ArrayList<>();
-        for (CatsCustomParameterBase parameter : namedMessageList) {
-            String namedMessageKey=parameter.getKey();
-            String messageToSend=assertStoryListData(namedMessageKey, String.class);
-            messageToSendList.add(messageToSend);
-        }
-        RemoteStepResult remoteStepResult= evaluate(
-                remoteStep("Sending message with using "+ namedWebSocket)
-                        .scriptOn(profileScriptResolver()
-                                        .map(
-                                                SendTextMessageAsIsParallel.class, BookableProfileName.websocket),
-                                requireProfile(reference.getProfileName()))
-                        .input(SendTextMessageAsIsParallel.IPARAM_ENDPOINTNAME, namedWebSocket)
-                        .input(SendTextMessageAsIsParallel.IPARAM_MESSAGETOSEND, (Serializable) messageToSendList)
-        );
-    }
+   @When("using the websocket $namedWebSocket the message named $namedMessage is sent as is with time stamp $namedTimeStamp")
+   public void sendMessageAsIsOverNamedWebSocketAndNameMessage( final String namedWebSocket, final String namedMessage,
+         final String namedTimeStamp )
+   {
+      final ProfileToWebSocketConfigurationReference reference =
+            getStoryListData( namedWebSocket, ProfileToWebSocketConfigurationReference.class );
+      final String messageToSend = assertStoryListData( namedMessage, String.class );
+      final RemoteStepResult remoteStepResult =
+            evaluate( remoteStep( "Sending message with using " + namedWebSocket )
+                  .scriptOn( profileScriptResolver().map( SendTextMessageAsIs.class, BookableProfileName.websocket ),
+                        requireProfile( reference.getProfileName() ) )
+                  .input( SendTextMessageAsIs.IPARAM_ENDPOINTNAME, namedWebSocket )
+                  .input( SendTextMessageAsIs.IPARAM_MESSAGETOSEND, messageToSend ) );
+      setStoryListData( namedTimeStamp, remoteStepResult.getOutput( SendTextMessageAsIs.OPARAM_ACTIONTIME ) );
 
-    @Then("using the websocket $namedWebSocket websocket message is received and validated against the expected message $expectedMessage with time stamp $namedTimeStamp")
-    public void receiveMessageAndValidate(final String namedWebSocket, final String expectedMessage, final String namedTimeStamp){
-        ProfileToWebSocketConfigurationReference reference = getStoryListData(namedWebSocket, ProfileToWebSocketConfigurationReference.class);
+   }
 
-        RemoteStepResult remoteStepResult= evaluate(
-                remoteStep("Sending message with using "+ namedWebSocket)
-                        .scriptOn(profileScriptResolver()
-                                        .map(
-                                                ReceiveMessageAsIs.class, BookableProfileName.websocket),
-                                requireProfile(reference.getProfileName()))
-                        .input(ReceiveMessageAsIs.IPARAM_ENDPOINTNAME,namedWebSocket)
-        );
-        String receviedMessage= (String) remoteStepResult.getOutput(ReceiveMessageAsIs.OPARAM_RECEIVEDMESSAGE);
-        String expectedMessageString=assertStoryListData(expectedMessage, String.class);
-        if(receviedMessage!=null) {
-            LocalStep checkReceivedMessage = localStep("Check received Message");
-            checkReceivedMessage.details(
-                    ExecutionDetails
-                            .create("Check received Message")
-                            .received(receviedMessage)
-                            .expected(expectedMessageString)
-                            .success(receviedMessage.contains(expectedMessageString))
-            );
-            setStoryListData(namedTimeStamp,remoteStepResult.getOutput(ReceiveMessageAsIs.OPARAM_ACTIONTIME));
-        }
-    }
+
+   @When("using the websocket $namedWebSocket the messages are sent: $namedMessageList")
+   public void sendMessagesAsIsOverNamedWebSocket( final String namedWebSocket,
+         final List<CatsCustomParameterBase> namedMessageList )
+   {
+      final ProfileToWebSocketConfigurationReference reference =
+            getStoryListData( namedWebSocket, ProfileToWebSocketConfigurationReference.class );
+      final List<String> messageToSendList = new ArrayList<>();
+      for ( final CatsCustomParameterBase parameter : namedMessageList )
+      {
+         final String namedMessageKey = parameter.getKey();
+         final String messageToSend = assertStoryListData( namedMessageKey, String.class );
+         messageToSendList.add( messageToSend );
+      }
+      final RemoteStepResult remoteStepResult =
+            evaluate(
+                  remoteStep( "Sending message with using " + namedWebSocket )
+                        .scriptOn( profileScriptResolver().map( SendTextMessageAsIsParallel.class,
+                              BookableProfileName.websocket ), requireProfile( reference.getProfileName() ) )
+                        .input( SendTextMessageAsIsParallel.IPARAM_ENDPOINTNAME, namedWebSocket ).input(
+                              SendTextMessageAsIsParallel.IPARAM_MESSAGETOSEND, ( Serializable ) messageToSendList ) );
+   }
+
+
+   @When("using the websocket $namedWebSocket the messages in group $group will be sent simultaneously")
+   public void sendMessagesAsIsOverNamedWebSocket( final String namedWebSocket, final String group )
+   {
+      final ProfileToWebSocketConfigurationReference reference =
+            getStoryListData( namedWebSocket, ProfileToWebSocketConfigurationReference.class );
+      ArrayList<String> messageToSendList = new ArrayList<>();
+      messageToSendList = assertStoryListData( group, ArrayList.class );
+
+      final RemoteStepResult remoteStepResult =
+            evaluate(
+                  remoteStep( "Sending message with using " + namedWebSocket )
+                        .scriptOn( profileScriptResolver().map( SendTextMessageAsIsParallel.class,
+                              BookableProfileName.websocket ), requireProfile( reference.getProfileName() ) )
+                        .input( SendTextMessageAsIsParallel.IPARAM_ENDPOINTNAME, namedWebSocket ).input(
+                              SendTextMessageAsIsParallel.IPARAM_MESSAGETOSEND, ( Serializable ) messageToSendList ) );
+
+   }
+
+
+   @Then("using the websocket $namedWebSocket websocket message is received and validated against the expected message $expectedMessage with time stamp $namedTimeStamp")
+   public void receiveMessageAndValidate( final String namedWebSocket, final String expectedMessage,
+         final String namedTimeStamp )
+   {
+      final ProfileToWebSocketConfigurationReference reference =
+            getStoryListData( namedWebSocket, ProfileToWebSocketConfigurationReference.class );
+
+      final RemoteStepResult remoteStepResult =
+            evaluate( remoteStep( "Sending message with using " + namedWebSocket )
+                  .scriptOn( profileScriptResolver().map( ReceiveMessageAsIs.class, BookableProfileName.websocket ),
+                        requireProfile( reference.getProfileName() ) )
+                  .input( ReceiveMessageAsIs.IPARAM_ENDPOINTNAME, namedWebSocket ) );
+      final String receviedMessage = ( String ) remoteStepResult.getOutput( ReceiveMessageAsIs.OPARAM_RECEIVEDMESSAGE );
+      final String expectedMessageString = assertStoryListData( expectedMessage, String.class );
+      if ( receviedMessage != null )
+      {
+         final LocalStep checkReceivedMessage = localStep( "Check received Message" );
+         checkReceivedMessage.details( ExecutionDetails.create( "Check received Message" ).received( receviedMessage )
+               .expected( expectedMessageString ).success( receviedMessage.contains( expectedMessageString ) ) );
+         setStoryListData( namedTimeStamp, remoteStepResult.getOutput( ReceiveMessageAsIs.OPARAM_ACTIONTIME ) );
+      }
+   }
 }
