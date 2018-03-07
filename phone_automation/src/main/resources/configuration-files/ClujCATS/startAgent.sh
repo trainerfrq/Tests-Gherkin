e138907a5255e6215189747a1a735248394090dd
spislar <serghei.pislar@frequentis.com>
2018-03-07 08:41:41 +0200

    QXVP-6708; fixed javafx profile

--- phone_automation/src/main/resources/configuration-files/ClujCATS/startAgent.sh
+++ phone_automation/src/main/resources/configuration-files/ClujCATS/startAgent.sh
@@ -1,2 +1,2 @@
 export cats_home=/opt/frequentis/shared/cats
-export JAVAPROP="-javaagent:/opt/frequentis/shared/cats/agent/cats-agent-testfx/4.2.0/cats-agent-testfx-4.2.0-isolated.jar=javafx/ClujCATS ${JAVAPROP}"
+export JAVAPROP="-javaagent:/opt/frequentis/shared/cats/agent/cats-agent-testfx/4.2.0/cats-agent-testfx-4.2.0-isolated.jar=javafx/hmi ${JAVAPROP}"
