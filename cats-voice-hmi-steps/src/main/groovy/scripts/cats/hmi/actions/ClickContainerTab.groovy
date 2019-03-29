6695a09d9e2b28a85a5db0bcc959e8fb5bcc8d5d
AMoraru <alexandra-ioana.moraru@frequentis.com>
2019-03-29 08:41:18 +0200

    QXVP-14704; added an alis for websocket tests (to avoid using the same steps naming like in the AG project)

---
+++ cats-voice-hmi-steps/src/main/groovy/scripts/cats/hmi/actions/ClickContainerTab.groovy
@@ -0,0 +1,34 @@
+package scripts.cats.hmi.actions
+
+import com.frequentis.c4i.test.model.ExecutionDetails
+import javafx.scene.Node
+import javafx.scene.layout.HBox
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import scripts.agent.testfx.automation.FxScriptTemplate
+
+class ClickContainerTab extends FxScriptTemplate {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClickContainerTab.class);
+    public static final String IPARAM_TAB_POSITION = "tab_position"
+
+    @Override
+    void script() {
+
+        Integer tabPosition = assertInput(IPARAM_TAB_POSITION) as Integer
+
+        Node mainGrid = robot.lookup("#daGrid_gridWidget #gridTabContainer").queryFirst();
+        HBox container = (HBox)mainGrid;
+
+        evaluate(ExecutionDetails.create("Verify container is found")
+                .received("container exists " )
+                .success(container != null));
+
+        if (container != null) {
+            ObservableList tabs =  container.getChildren()
+            List<Node> tabsList = new ArrayList<>(tabs)
+            Node tab = tabsList.get(tabPosition)
+            robot.clickOn(robot.point(tab))
+        }
+
+    }
+}
