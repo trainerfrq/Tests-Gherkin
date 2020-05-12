9e7a62a602c2477caaf5d30ed542c920f434947f
AMoraru <alexandra-ioana.moraru@frequentis.com>
2020-05-12 08:26:41 +0300

    PVCSX-4235; New steps

--- phone_automation/src/main/java/com/frequentis/xvp/voice/test/automation/phone/step/local/ATISteps.java
+++ phone_automation/src/main/java/com/frequentis/xvp/voice/test/automation/phone/step/local/ATISteps.java
@@ -2,6 +2,7 @@ package com.frequentis.xvp.voice.test.automation.phone.step.local;
 
 import com.frequentis.c4i.test.bdd.fluent.step.AutomationSteps;
 import com.frequentis.c4i.test.bdd.fluent.step.local.LocalStep;
+import com.frequentis.c4i.test.model.ExecutionDetails;
 import com.frequentis.xvp.voice.if73.json.messages.JsonMessage;
 import com.frequentis.xvp.voice.if73.json.messages.payload.common_commands.Command;
 import com.frequentis.xvp.voice.if73.json.messages.payload.common_commands.CommandResponse;
@@ -12,12 +13,15 @@ import com.frequentis.xvp.voice.if73.json.utils.GsonUtils;
 import com.google.gson.Gson;
 import org.glassfish.jersey.client.JerseyClientBuilder;
 import org.glassfish.jersey.client.JerseyWebTarget;
+import org.jbehave.core.annotations.Alias;
+import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 import java.util.UUID;
 
 import static com.frequentis.c4i.test.model.MatcherDetails.match;
@@ -78,6 +82,124 @@ public class ATISteps extends AutomationSteps
                 .details( match( output.getResult(), equalTo(Result.OK)))
                 .details( match( output.getId(), equalTo(missionName)) ));
 
+    }
+
+    @Then("$hmiOperator verifies (via POST request) change mission $missionName was successfully")
+    public void apiVerifyChangeMission( final String hmiOperator, final String missionName )
+            throws Throwable
+    {
+        final LocalStep localStep = localStep( "Execute POST request - verify mission change" );
+
+        String  endpointUri = getStoryListData(hmiOperator, String.class);
+
+        final JsonMessage jsonMessageRequest =
+                JsonMessage.builder()
+                        .withCorrelationId(UUID.randomUUID())
+                        .withPayload( new Command(null, CommandType.MISSION_STATUS )).build();
+
+        Response response =
+                getATIWebTarget( endpointUri )
+                        .request( MediaType.APPLICATION_JSON )
+                        .post( Entity.json( jsonMessageRequest.toJson() ) );
+
+        final CommandResponse output = getResponseContent(response);
+
+        evaluate( localStep( "Verify response for executed POST request - verify mission change" )
+                .details( match( output.getCommandType(), equalTo(CommandType.MISSION_STATUS)) )
+                .details( match( output.getResult(), equalTo(Result.OK)))
+                .details( match( output.getId(), equalTo(missionName)) ));
+
+    }
+
+    @Then("$hmiOperator verify (via POST request) that DA key $target has status $status")
+    public void apiDACallStatus( final String hmiOperator, final String target, final String status )
+            throws Throwable
+    {
+        final LocalStep localStep = localStep( "Execute POST request - verify DA call status" );
+
+        String  endpointUri = getStoryListData(hmiOperator, String.class);
+
+        final JsonMessage jsonMessage =
+                JsonMessage.builder()
+                        .withCorrelationId(UUID.randomUUID())
+                        .withPayload( new GgCommand( target, GgCommandType.GET_CALL_STATUS )
+                        .withCallType( GgCallType.DA ) ).build();
+
+        Response response =
+                getATIWebTarget( endpointUri )
+                        .request( MediaType.APPLICATION_JSON )
+                        .post( Entity.json( jsonMessage.toString() ) );
+
+        final GgCommandResponse output = getGgResponseContent(response);
+        List<GgCallStatusElement> elements = output.getData();
+
+        for(GgCallStatusElement element : elements){
+            if(element.getId().contains(target)) {
+                evaluate(localStep("Verify response for executed POST request - verify DA call status")
+                        .details(match(output.getCommandType(), equalTo(GgCommandType.GET_CALL_STATUS)))
+                        .details(match(output.getResult(), equalTo(GgResult.OK)))
+                        .details(match(output.getId(), equalTo(target)))
+                        .details(match(element.getStatus().toString(), equalTo(status))));
+            }
+            break;
+        }
+    }
+
+    @Then("$hmiOperator verify (via POST request) that call queue has status $status")
+    public void apiCallQueueStatus( final String hmiOperator, final String status )
+            throws Throwable
+    {
+        final LocalStep localStep = localStep( "Execute POST request - verify call queue status" );
+
+        String  endpointUri = getStoryListData(hmiOperator, String.class);
+
+        final JsonMessage jsonMessage =
+                JsonMessage.builder()
+                        .withCorrelationId(UUID.randomUUID())
+                        .withPayload( new GgCommand( "callQueue", GgCommandType.GET_CALL_STATUS )).build();
+
+        Response response =
+                getATIWebTarget( endpointUri )
+                        .request( MediaType.APPLICATION_JSON )
+                        .post( Entity.json( jsonMessage.toString() ) );
+
+        final GgCommandResponse output = getGgResponseContent(response);
+        List<GgCallStatusElement> elements = output.getData();
+
+        for(GgCallStatusElement element : elements){
+                evaluate(localStep("Verify response for executed POST request - verify call queue status")
+                        .details(match(output.getCommandType(), equalTo(GgCommandType.GET_CALL_STATUS)))
+                        .details(match(output.getResult(), equalTo(GgResult.OK)))
+                        .details(match(output.getId(), equalTo("callQueue")))
+                        .details(match(element.getStatus().toString(), equalTo(status))));
+        }
+    }
+
+    @When("$hmiOperator start (via POST request) a call from phone book to $target")
+    public void apiPhonebookCall( final String hmiOperator, final String target )
+            throws Throwable
+    {
+        final LocalStep localStep = localStep( "Execute POST request - phone book call" );
+
+        String  endpointUri = getStoryListData(hmiOperator, String.class);
+
+        final JsonMessage jsonMessage =
+                JsonMessage.builder()
+                        .withCorrelationId(UUID.randomUUID())
+                        .withPayload( new GgCommand( target, GgCommandType.PHONEBOOK_CALL )).build();
+
+        Response response =
+                getATIWebTarget( endpointUri )
+                        .request( MediaType.APPLICATION_JSON )
+                        .post( Entity.json( jsonMessage.toString() ) );
+
+        final GgCommandResponse output = getGgResponseContent(response);
+
+        evaluate( localStep( "Verify response for executed POST request - phone book call" )
+                .details( match( output.getCommandType(), equalTo(GgCommandType.PHONEBOOK_CALL)) )
+                .details( match( output.getResult(), equalTo(GgResult.OK)))
+                .details( match( output.getId(), equalTo(target)) )
+                .details( match(output.getData(), is(nullValue())) ));
     }
 
     private JerseyWebTarget getATIWebTarget(final String uri )
--- phone_automation/src/main/stories/voice_GG/ui/Endurance/ExampleStoryHMIAPI.story
+++ phone_automation/src/main/stories/voice_GG/ui/Endurance/ExampleStoryHMIAPI.story
@@ -5,6 +5,16 @@ As a user
 I want to perform an action
 So that I can achieve a business goal
 
+Scenario: Booking profiles
+Given booked profiles:
+| voip/<<systemName>> | <<systemName>> | <<CO3_IP>>     | VOIP       |
+
+Scenario: Create sip phone
+Given SipContacts group SipContact:
+| key        | profile | user-entity | sip-uri        |
+| SipContact | VOIP    | 12345       | <<SIP_PHONE2>> |
+And phones for SipContact are created
+
 Scenario: Define call source and API URI
 When define values in story data:
 | name    | value            |
@@ -12,7 +22,53 @@ When define values in story data:
 | HMI OP2 | <<HMI2_API.URI>> |
 | HMI OP3 | <<HMI3_API.URI>> |
 
-Scenario: scenario description
-When HMI OP1 changes (via POST request) current mission to mission WEST-EXEC
-When HMI OP1 presses (via POST request) DA key role1
+Scenario: Operator1 changes the mission on HMI screen
+When HMI OP1 changes (via POST request) current mission to mission <<MISSION_1_NAME>>
+Then HMI OP1 verifies (via POST request) change mission <<MISSION_1_NAME>> was successfully
 
+Scenario: Operator2 changes the mission on HMI screen
+When HMI OP2 changes (via POST request) current mission to mission <<MISSION_2_NAME>>
+Then HMI OP2 verifies (via POST request) change mission <<MISSION_2_NAME>> was successfully
+
+Scenario: Steps 1 and 2 described in the Narrative
+When HMI OP1 presses (via POST request) DA key <<ROLE_2_NAME>>
+Scenario: Steps 1 and 2 described in the Narrative
+Then HMI OP1 verify (via POST request) that DA key <<ROLE_2_NAME>> has status OUTGOING
+Scenario: Steps 1 and 2 described in the Narrative
+Then HMI OP1 verify (via POST request) that call queue has status OUTGOING
+Scenario: Steps 1 and 2 described in the Narrative
+!-- Then HMI OP2 verify (via POST request) that call queue has status RINGING
+Then HMI OP2 verify (via POST request) that DA key <<ROLE_1_NAME>> has status RINGING
+Scenario: Steps 1 and 2 described in the Narrative
+When HMI OP2 presses (via POST request) DA key <<ROLE_1_NAME>>
+Scenario: Steps 1 and 2 described in the Narrative
+Then HMI OP1 verify (via POST request) that call queue has status ESTABLISHED
+Scenario: Steps 1 and 2 described in the Narrative
+Then HMI OP2 verify (via POST request) that call queue has status ESTABLISHED
+Then waiting for 10 seconds
+When HMI OP1 presses (via POST request) DA key <<ROLE_2_NAME>>
+Then HMI OP2 verify (via POST request) that call queue has status TERMINATED
+Then HMI OP1 verify (via POST request) that call queue has status TERMINATED
+Then waiting for 5 seconds
+When HMI OP2 presses (via POST request) DA key <<ROLE_1_NAME>>
+Then HMI OP2 verify (via POST request) that DA key <<ROLE_1_NAME>> has status OUTGOING
+Then HMI OP2 verify (via POST request) that call queue has status OUTGOING
+Then HMI OP1 verify (via POST request) that call queue has status RINGING
+When HMI OP1 presses (via POST request) DA key <<ROLE_2_NAME>>
+Then HMI OP1 verify (via POST request) that call queue has status ESTABLISHED
+Then HMI OP2 verify (via POST request) that call queue has status ESTABLISHED
+Then waiting for 10 seconds
+When HMI OP2 presses (via POST request) DA key <<ROLE_1_NAME>>
+Then HMI OP2 verify (via POST request) that call queue has status TERMINATED
+Then HMI OP1 verify (via POST request) that call queue has status TERMINATED
+Then waiting for 5 seconds
+Scenario: Steps 1 and 2 described in the Narrative
+When HMI OP1 start (via POST request) a call from phone book to Madoline
+Scenario: Steps 1 and 2 described in the Narrative
+Then HMI OP1 verify (via POST request) that call queue has status OUTGOING
+When SipContact answers incoming calls
+Then HMI OP1 verify (via POST request) that call queue has status ESTABLISHED
+Then waiting for 10 seconds
+When SipContact terminates calls
+Then HMI OP1 has in the call queue a number of 0 calls
+Then HMI OP1 verify (via POST request) that call queue has status TERMINATED
